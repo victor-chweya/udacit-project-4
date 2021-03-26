54e33d088c7f97d439dd82dc0af51410b4d4fd4d
Victor <vchweya@avicdesign.com>
2021-03-26 17:47:35 +0300

    updated the app to use latest webpack and packages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,33 +1,11 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/client/index.js',
+module.exports = merge(common, {
     mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
-        ]
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
         new MiniCssExtractPlugin({filename: '[name].css'})
     ]
-}
+});
+
